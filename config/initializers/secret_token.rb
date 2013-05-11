7eab4b9238b49e6e6d673d642839e29a6b986d5a
Gregory Jean-Baptiste <gjean011@yahoo.com>
2013-05-11 10:37:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SueMe::Application.config.secret_token = 'da9ef3f48e9e8ffd798d51be1c3eb082b5e04a57b89ea8a6f22d7eba62b5b81ac79d3c1159c9b2c59991f5b5a7d83711637d337278295a42812fbf353caf6e5b'
